2ce57214299167c61aa242e81b25f9cfd29bbe90
osamasayd <osamasayd141@gmail.com>
2023-12-28 23:26:04 +0300

    .

--- YouTube.py
+++ YouTube.py
@@ -1,9 +1,8 @@
 # import os
 # os.environ.get('/opt/render/.local/lib/python3.7/site-packages')
+# sys.path.append('/opt/render/.local/lib/python3.7/site-packages')
+from pytube import YouTube
 import sys
-sys.path.append('/opt/render/.local/lib/python3.7/site-packages')
-# from pytube 
-import YouTube
 
 link = sys.argv[1]
 
