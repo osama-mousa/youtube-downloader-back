a63d33b94b46ed023d499e8db082795486af3925
osamasayd <osamasayd141@gmail.com>
2023-12-31 22:19:55 +0300

    recode

--- app.js
+++ app.js
@@ -1,66 +1,60 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const { exec } = require('child_process');
-const path = require('path');
-const fs = require('fs');
-const ytdl = require('ytdl-core');
-
-// Disable update check for ytdl-core
-process.env.YTDL_NO_UPDATE = true;
+const cors = require("cors");
+const { exec } = require("child_process");
+const path = require("path");
+const fs = require("fs");
 
 app.use(cors());
 app.use((req, res, next) => {
-  res.header('Content-Type', 'application/json');
+  res.header("Content-Type", "application/json");
   next();
 });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
-  console.log('I am Listening in port 3000');
+  console.log("I am Listening in port 3000");
 });
 
 const links = [];
 
-app.post('/api/downloadVideo', async (req, res) => {
+app.post("/api/downloadVideo", async (req, res) => {
   const link = req.body.link;
   links.push(link);
   try {
-    const videoInfo = await ytdl.getInfo(link);
-    const format = ytdl.chooseFormat(videoInfo.formats, { quality: 'highest' });
+    const filePath = await downloadVideo(link);
+    const filename = path.basename(filePath);
+    const absolutePath = path.resolve(filePath);
+    console.log(`Video downloaded successfully: ${filename}`);
 
-    if (!format) {
-      console.error('No suitable format found');
-      res.status(500).send('Internal Server Error');
-      return;
-    }
-
-    const videoStream = ytdl(link, { quality: 'highest' });
-
-    const filename = `${videoInfo.title}.mp4`;
-    const filePath = path.join(__dirname, filename);
-    const file = fs.createWriteStream(filePath);
-
-    videoStream.pipe(file);
-
-    file.on('finish', () => {
-      console.log(`Video downloaded successfully: ${filename}`);
-
-      res.setHeader('Content-Type', 'video/mp4');
-      res.sendFile(filePath, filename, (err) => {
-        if (err) {
-          console.error(`Error sending file: ${err}`);
-          res.status(500).send('Internal Server Error');
-        } else {
-          console.log('File sent successfully');
-          fs.unlinkSync(filePath);
-        }
-      });
+    // إرسال الملف كاستجابة
+    res.setHeader("Content-Type", "video/mp4");
+    res.download(absolutePath, filename, (err) => {
+      if (err) {
+        console.error(`Error sending file: ${err}`);
+        res.status(500).send("Internal Server Error");
+      } else {
+        console.log("File sent successfully");
+        fs.unlinkSync(absolutePath);
+      }
     });
-
   } catch (error) {
-    console.error(`Error downloading video: ${error.message}`);
-    res.status(404).send('The download link not found.');
+    console.error(`Error downloading video: ${error}`);
+    res.status(404).send("The download link not found.");
   }
 });
+
+function downloadVideo(link) {
+  return new Promise((resolve, reject) => {
+    const command = `python YouTube.py ${link}`;
+
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(stdout.trim());
+      }
+    });
+  });
+}
