03db890e42b7cd53e30562e55a246a6be346040e
osamasayd <osamasayd141@gmail.com>
2024-01-08 01:13:43 +0300

    .

---
+++ YouTube.py
@@ -0,0 +1,13 @@
+from pytube import YouTube
+import sys
+
+link = sys.argv[1]
+
+try:
+    video = YouTube(link)
+    stream = video.streams.get_highest_resolution()
+    file_path = stream.download()
+    print(f"{file_path}")
+except Exception as e:
+    print(f"Error downloading video: {e}")
+    sys.exit(1)
